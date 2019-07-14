6e07083c236a673ae1c0976c4aac8d150296759e
IskanderB <axurtin.rep@gmail.com>
2019-07-14 16:18:41 +0300

    Deploy

--- RatePars/settings.py
+++ RatePars/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g0_d3lc4(!=iea$m(qmacl4mdp!eehpu3+mn((_4dj7q8pih%&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
