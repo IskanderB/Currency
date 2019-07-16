2228c35d32d1aa86ba0a4b3858d18b8081be86ec
IskanderB <axurtin.rep@gmail.com>
2019-07-16 21:06:45 +0300

    New commit

--- RatePars/settings.py
+++ RatePars/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import psycopg2
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'RatePars.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'our_db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -124,6 +125,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import dj_database_url
-
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
