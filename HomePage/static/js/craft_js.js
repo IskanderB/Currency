63573c44a1cd016739105d7060afa10864421490
IskanderB <axurtin.rep@gmail.com>
2019-08-16 21:16:12 +0300

    version 2

--- HomePage/static/js/craft_js.js
+++ HomePage/static/js/craft_js.js
@@ -33,3 +33,50 @@ $("#button_follow").click(function() {
 	}
 	});
 });
+
+
+var start_1 = true;         //Vars for change over inputs when it fill to click on string of table
+var start_2 = false;
+
+
+
+var inputs_2 = document.getElementsByTagName("tr");   //Get teg elements "tr"
+	for (var i = 0; i < inputs_2.length; i++) {       //Search teg elements "tr" you clicked
+	  inputs_2[i].addEventListener("click", myFunction_2);
+	}
+
+	function myFunction_2() {
+		//var z=document.getElementById("first_cur");
+		//z.innerHTML=this.id;
+		
+		if((start_2)&&(this.id)){         //Fill id input #second_cur currency cod you clicked 
+		$('#second_cur').val(this.id);
+		$("#second_cur").attr("placeholder", this.id);
+		console.log(this.id)
+		start_2 = false;
+		start_1 = false;
+	}
+		else start_1 = true;
+	    //alert(this.id);
+	}
+
+
+
+var inputs = document.getElementsByTagName("tr");
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("click", myFunction);
+}
+
+function myFunction() {
+	//var z=document.getElementById("first_cur");
+	//z.innerHTML=this.id;
+
+	if((start_1)&&(this.id)){           //Fill id input #first_cur currency cod you clicked 
+	$('#first_cur').val(this.id);
+	$("#first_cur").attr("placeholder", this.id);
+	console.log(this.id)
+	start_2 = true;
+}
+    //alert(this.id);
+    
+}
